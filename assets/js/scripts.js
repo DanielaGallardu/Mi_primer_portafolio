1f4c7058e2de0ba70890b3765c31992cc23c6205
Daniela Gallardo <daniela.gallardoco@gmail.com>
2024-07-16 19:51:11 -0400

    Parte final

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
