79de6692b14f50a2d99fc32eb2037f418e748ee9
Daniela Gallardo <daniela.gallardoco@gmail.com>
2024-07-16 19:53:00 -0400

    Archivo no usado

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-const myModal = document.getElementById('loginModal') // Acá buscamos modal
-const myInput = document.getElementById('email') // Acá buscamos input en formulario de modal
-
-
-// Activamos el focus en el input del modal
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
